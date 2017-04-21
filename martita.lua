50d2f1657cc68f9c9b620a07ede82d6e782f0465
Ferran Gonzalez Alonso <gaferran@gmail.com>
2017-04-21 02:15:03 -0300

    Atualizado loop pra minuto

--- martita.lua
+++ martita.lua
@@ -10,13 +10,18 @@ pin_dht = 1
 url = 'https://martita-50f93.firebaseio.com/martita.json'
 content_type = 'Content-Type: application/json\r\n'
     
-if not tmr.alarm(0, 10000, tmr.ALARM_AUTO, function()
+if not tmr.alarm(0, 60000, tmr.ALARM_AUTO, function()
     status, temperatura, umidade = dht.read(pin_dht)
     valor_solo = adc.read(0)
-    solo = map(valor_solo , 1023, 0, 0, 100)
     solo = (valor_solo - 1023) * (100 - 0) / (0 - 1023) + 0
-    
     json = '{"temperatura":"'..temperatura..'", "umidade":'..umidade..', "solo":'..solo..'}'
-    http.post(url,content_type,json)
+    http.post(url, content_type, json,
+    function(code, data)
+        if (code < 0) then
+            print("HTTP request failed")
+        else
+            print(code, data)
+        end
+    end)
 end)
 then end
